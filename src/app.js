f3572ed5f2167fadb709e6fd6a44369dcde55410
Kardem11 <simgecimm@hotmail.de>
2022-12-22 20:24:41 +0300

    .

---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log("Hello JavaScript")
+
+//Js isn't Typesafe..
+
+//array
+let fruits = ["Apple","Banana","Peach",22,["Apricot","Grape"]]
+
+console.log("</ul>")
+for (let i = 0; i<fruits.length; i++){
+    console.log("</li>"+fruits[i]+"</li>")
+}
+console.log("</ul>")
+console.log(fruits)
+
